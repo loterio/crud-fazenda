ac32441bc7964d8bf9c4165fc013dbabbc0e301b
fabioLoterio <vitorloterio@gmail.com>
2020-02-13 12:48:40 -0300

    putting the conf folder to connect with db

---
+++ conf/conf.inc.php
@@ -0,0 +1,14 @@
+<?php
+    // Banco de Dados
+    define('HOST', 'localhost');  
+    define('DBNAME', '');    
+    define('USER', 'root');  
+    define('PASSWORD', '');
+
+    define('DRIVER', 'mysql'); 
+    define('CHARSET', 'utf8');
+
+    // Geral da Aplicação
+    define('NOME_DO_PROJETO','CRUD-01-R');
+    define('DESCRICAO_DO_PROJETO','Um Pequeno exemplo usando PDO');
+?>
